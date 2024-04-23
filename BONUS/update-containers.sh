13eed58ceaa5b3b638e9048b7e677d609e417eff
MishkaFloof <sutton.98@wright.edu>
2024-04-22 22:00:40 -0400

    Update update-containers.sh

--- BONUS/update-containers.sh
+++ BONUS/update-containers.sh
@@ -3,5 +3,5 @@
 # Pull the latest image from DockerHub
 docker-compose -f /home/ubuntu/s24cicd-MishkaFloof/BONUS/BONUSTemplate.yml pull
 
-# Restart Docker containers
+# Restart Docker containers, including load balancer
 docker-compose -f /home/ubuntu/s24cicd-MishkaFloof/BONUS/BONUSTemplate.yml restart
